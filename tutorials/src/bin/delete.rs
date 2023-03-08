d291c571427172eadfa30678a0317d9bd416533d
pauldelucia <pauldelucia2@gmail.com>
2023-03-08 20:06:18 +0700

    make delete tutorial independent

--- tutorials/src/bin/delete.rs
+++ tutorials/src/bin/delete.rs
@@ -1,13 +1,28 @@
 use grovedb::GroveDb;
+use grovedb::Element;
 
 fn main() {
     // Specify a path and open GroveDB at the path as db
     let path = String::from("../storage");
     let db = GroveDb::open(path).unwrap();
 
-    // Check the key-values are there
+    // Define key-values for insertion
     let key1 = b"hello";
+    let val1 = b"world";
     let key2 = b"grovedb";
+    let val2 = b"rocks";
+
+    // Insert key-value 1 into the root tree
+    db.insert([], key1, Element::Item(val1.to_vec(), None), None, None)
+        .unwrap()
+        .expect("successful key1 insert");
+
+    // Insert key-value 2 into the root tree
+    db.insert([], key2, Element::Item(val2.to_vec(), None), None, None)
+        .unwrap()
+        .expect("successful key2 insert");
+
+    // Check the key-values are there
     let result1 = db.get([], key1, None).unwrap();
     let result2 = db.get([], key2, None).unwrap();
     println!("Before deleting, we have key1: {:?}", result1);
