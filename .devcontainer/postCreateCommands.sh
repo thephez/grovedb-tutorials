5686e2ad9cf340bb9361acec7675c74bedec0223
thephez <thephez@users.noreply.github.com>
2023-05-01 19:14:19 +0000

    build: add post create script to install required stuff

---
+++ .devcontainer/postCreateCommands.sh
@@ -0,0 +1,11 @@
+#/!bin/sh
+
+# Command to run during postCreateCommand
+# Done in script because when done via object the commands are run in parallel
+#  (which isn't always desirable)
+# https://containers.dev/implementors/spec/#parallel-exec
+# https://containers.dev/implementors/json_reference/#formatting-string-vs-array-properties
+
+# Needed to build
+sudo apt update
+sudo apt install libclang-dev
